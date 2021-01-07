9f11901d006dcb0ed3ca5866be1b27da2c335259
TanHM-1211 <zaytpotter@gmail.com>
2021-01-07 18:26:41 +0700

    remove reduntdant

--- model/base_seq2seq.py
+++ model/base_seq2seq.py
@@ -214,3 +214,163 @@ class Seq2SeqModel(nn.Module):
     #
     #     return torch.LongTensor(res[0][0][:-1]).to(self.device)
 
+# class Encoder(nn.Module):
+#     def __init__(self, embedding: nn.Embedding, lstm_dim, bidirectional=True, num_layers=2):
+#         super(Encoder, self).__init__()
+#         self.embedding = embedding
+#
+#         self.lstm_dim = lstm_dim
+#         self.bidirectional = bidirectional
+#         self.direction = 1 if not self.bidirectional else 2
+#         self.num_layers = num_layers
+#         self.lstm = nn.LSTM(self.embedding.embedding_dim, self.lstm_dim, batch_first=True, bidirectional=self.bidirectional,
+#                                num_layers=self.num_layers)
+#
+#     def forward(self, inputs, hidden=None):
+#
+#         lens = [len(sent) for sent in inputs]
+#
+#         # padding
+#         inputs = pad_sequence(inputs, batch_first=True, padding_value=self.embedding.padding_idx)
+#         inputs = self.embedding(inputs)  # shape: batch * max(lens) * embedding_dim
+#
+#         # packing
+#         inputs = pack_padded_sequence(inputs, lens, batch_first=True, enforce_sorted=False)
+#
+#         output, (h, c) = self.lstm(inputs, hidden)
+#         h = h.reshape(self.num_layers, self.direction, len(lens), -1).transpose(2, 1).reshape(self.num_layers,
+#                                                                                               len(lens), -1)
+#         c = c.reshape(self.num_layers, self.direction, len(lens), -1).transpose(2, 1).reshape(self.num_layers,
+#                                                                                               len(lens), -1)
+#
+#         return output, (h, c)
+#
+#
+# class Decoder(nn.Module):
+#     def __init__(self, embedding: nn.Embedding, lstm_dim, num_layers=1, dropout=0.1):
+#         super(Decoder, self).__init__()
+#         self.embedding = embedding
+#
+#         self.lstm_dim = lstm_dim
+#         self.num_layers = num_layers
+#         self.network = nn.LSTM(self.embedding.embedding_dim, self.lstm_dim, batch_first=True,
+#                                num_layers=self.num_layers)
+#         self.linear = nn.Linear(self.lstm_dim, self.embedding.num_embeddings)
+#         self.dropout = nn.Dropout(dropout)
+#
+#     def forward(self, inputs, hidden_states):
+#         decoder_inputs = inputs
+#
+#         decoder_inputs_lens = [len(sent) for sent in decoder_inputs]
+#         decoder_inputs = pad_sequence(decoder_inputs, batch_first=True,
+#                                       padding_value=self.embedding.padding_idx)
+#
+#         decoder_inputs = self.embedding(decoder_inputs)
+#
+#         decoder_inputs = pack_padded_sequence(decoder_inputs, decoder_inputs_lens, batch_first=True,
+#                                               enforce_sorted=False)
+#         out_packed, hidden_states = self.network(decoder_inputs, hidden_states)
+#         # unpack
+#         out, lens_unpack = pad_packed_sequence(out_packed, batch_first=True,
+#                                                padding_value=self.embedding.padding_idx)
+#         # linear forward
+#
+#         return out
+#
+#
+# class BaseSeq2SeqModel(nn.Module):
+#     def __init__(self, encoder: Encoder, decoder: Decoder, bos_idx: int = 0, eos_idx: int = 2, device='cuda'):
+#         super(BaseSeq2SeqModel, self).__init__()
+#         self.encoder = encoder
+#         self.decoder = decoder
+#         self.bos_idx = bos_idx
+#         self.eos_idx = eos_idx
+#
+#         self.softmax = nn.Softmax()
+#         self.loss = nn.CrossEntropyLoss()
+#         self.device = device
+#
+#     def forward(self, encoder_inputs, decoder_inputs):
+#         encoder_outputs, hidden_states = self.encoder(encoder_inputs)
+#
+#         decoder_outputs = self.decoder(decoder_inputs, hidden_states)
+#
+#         return decoder_outputs
+#
+#     def forward_with_loss(self, encoder_inputs, decoder_inputs, decoder_decoder_inputss):
+#         decoder_outputs = self.forward(encoder_inputs, decoder_inputs).transpose(2, 1)
+#
+#         return self.loss(decoder_outputs, decoder_decoder_inputss)
+#
+#     def predict(self, sentences, max_len=20, beam_size=5):
+#         encoder_outputs, hidden_states = self.encoder(sentences)
+#
+#         # h_n of shape (num_layers, batch, hidden_size * num_directions)
+#         result = []
+#         h, c = hidden_states
+#         for i in range(len(sentences)):
+#             h_i = h[:, i: i + 1, :]
+#             c_i = c[:, i: i + 1, :]
+#             result.append(self.beam_search((h_i.contiguous(), c_i.contiguous()), max_len=max_len, beam_size=beam_size))
+#         return result
+#
+#     def forward_one_token(self, token, states, beam_size=None):
+#         if beam_size is None:
+#             beam_size = self.beam_size
+#         input_id = torch.LongTensor([[token]]).to(self.device)
+#         output, states = self.decoder(input_id, states)
+#         topk_output = torch.topk(self.softmax(output.squeeze(0).squeeze(0)), k=beam_size, dim=-1)
+#         topk_output_indices = topk_output.indices.tolist()  # for next token
+#         topk_output_values = topk_output.values.tolist()  # for probability of next token
+#
+#         return topk_output_indices, topk_output_values, states
+#
+#     def normalize_prob(self, prob):
+#         return np.log(0.5 + prob)
+#
+#     def search_token(self, token, states, beam_size=5):
+#         if self.device == 'cuda':
+#             input_id = torch.LongTensor([[token]]).cuda()
+#         else:
+#             input_id = torch.LongTensor([[token]]).to(self.device)
+#
+#         output, states = self.decoder(input_id, states)
+#         topk_output = torch.topk(self.softmax(output.squeeze(0).squeeze(0)), k=beam_size, dim=-1)
+#         topk_output_indices = topk_output.indices.tolist()  # for next token
+#         topk_output_values = topk_output.values.tolist()  # for probability of next token
+#
+#         return topk_output_indices, topk_output_values, states
+#
+#     def beam_search(self, hidden_states, max_len=20, beam_size=5):
+#         # initialize
+#         topk_output_indices, topk_output_values, hidden_states = self.forward_one_token(self.bos_idx, hidden_states,
+#                                                                                         beam_size)
+#         res = []
+#         for i in range(len(topk_output_indices)):
+#             res.append([[topk_output_indices[i]], self.normalize_prob(topk_output_values[i]), hidden_states])
+#
+#         while True:
+#             candidates = []
+#             count_eos_token = 0
+#             for pos in range(len(res)):
+#                 input_ids, accumulate_prob, hidden_states = res[pos]
+#                 input_id = input_ids[-1]
+#                 print(len(input_ids))
+#                 if input_id != self.eos_idx and len(input_ids) < max_len:
+#                     topk_output_indices, topk_output_values, new_hidden_states = self.search_token(self.bos_idx, hidden_states,
+#                                                                                                  beam_size)
+#                     for i in range(len(topk_output_indices)):
+#                         candidates.append([input_ids + [topk_output_indices[i]],
+#                                            (accumulate_prob + self.normalize_prob(topk_output_values[i])),
+#                                            # normalize with len
+#                                            new_hidden_states])
+#                 elif input_id == self.eos_idx:
+#                     count_eos_token += 1
+#                 else:
+#                     input_ids.append(self.eos_idx)
+#             if count_eos_token == beam_size or len(candidates) == 0:
+#                 break
+#             candidates.sort(key=lambda x: x[1], reverse=True)
+#             res = candidates[:beam_size]
+#
+#         return np.array(res[0][0])
